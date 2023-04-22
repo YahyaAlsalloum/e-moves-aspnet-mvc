8a72976287269d468ea625ac7afab7aba7099c96
MadSands <MadSands@DESKTOP-6DPEM54>
2023-04-22 09:40:36 +0300

    Add project files.

---
+++ e-moves/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_moves.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
